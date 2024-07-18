47984ea43ef1d0a9f5da84ad7c94460fdc46083e
krittakondev <krit@9krit.dev>
2024-07-18 17:13:48 +0700

    perf: move try except

--- screen.py
+++ screen.py
@@ -35,59 +35,52 @@ tmp_check = ""
 step_anime = 0
 def fetch_screen(text, stat):
     print(os.environ.get("env_screen"))
-    try:
-        now = datetime.now()
-        time_str = now.strftime('%H:%M:%S')
-        date_str = now.strftime('%Y-%m-%d')
+    now = datetime.now()
+    time_str = now.strftime('%H:%M:%S')
+    date_str = now.strftime('%Y-%m-%d')
 
-        
-        image = Image.new('1', (epd.height, epd.width), 255)  # 255: สีขาว
-        #bg_black = Image.new('1', (epd.height-20, epd.width), 0)  
-        #image.paste(bg_black, (0,20))
-
-        check = time_str+date_str+text+str(stat)
-        draw = ImageDraw.Draw(image)
-        #draw.text((120, 50), "ดีจ้าาาา", font=font15, fill=255)  
-        #global tmp_check 
-        #if check != tmp_check:
-        icon = Image.open(dirname+"/icon.png").convert('1')
-        
-        tmp_check = check
-        print(epd.height, epd.width)
     
-        draw.text((2, 1), time_str, font=font, fill=0)  # แสดงเวลา
-        draw.text((80, 1), date_str, font=font, fill=0)  # แสดงวันที่
-        draw.text((220, 1), text, font=font, fill=0)  
-        
-        if stat:
-            icon_width, icon_height = icon.size
-            #image.paste(icon, (epd.height - 1 - icon_width, 1))
-            image.paste(icon, (185, -10))
+    image = Image.new('1', (epd.height, epd.width), 255)  # 255: สีขาว
+    #bg_black = Image.new('1', (epd.height-20, epd.width), 0)  
+    #image.paste(bg_black, (0,20))
 
-            #epd.display_fast(epd.getbuffer(image))
+    check = time_str+date_str+text+str(stat)
+    draw = ImageDraw.Draw(image)
+    #draw.text((120, 50), "ดีจ้าาาา", font=font15, fill=255)  
+    #global tmp_check 
+    #if check != tmp_check:
+    icon = Image.open(dirname+"/icon.png").convert('1')
+    
+    tmp_check = check
+    print(epd.height, epd.width)
 
-        global step_anime
-        if step_anime > 23:
-            step_anime = 0
-        anime_img = Image.open(f"{dirname}/pic/enterprise-confused-{step_anime}.bmp").convert('1')
-        image.paste(anime_img, (0,20))
-        step_anime += 1
+    draw.text((2, 1), time_str, font=font, fill=0)  # แสดงเวลา
+    draw.text((80, 1), date_str, font=font, fill=0)  # แสดงวันที่
+    draw.text((220, 1), text, font=font, fill=0)  
+    
+    if stat:
+        icon_width, icon_height = icon.size
+        #image.paste(icon, (epd.height - 1 - icon_width, 1))
+        image.paste(icon, (185, -10))
+
+        #epd.display_fast(epd.getbuffer(image))
+
+    global step_anime
+    if step_anime > 23:
+        step_anime = 0
+    anime_img = Image.open(f"{dirname}/pic/enterprise-confused-{step_anime}.bmp").convert('1')
+    image.paste(anime_img, (0,20))
+    step_anime += 1
 
 
-        cpu_per = psutil.cpu_percent()
-        draw.text((10, 30), f"CPU: {cpu_per}%", font=font, fill=255)  
-        draw.text((10, 50), f"MEM: {psutil.virtual_memory().percent}%", font=font, fill=255)  
-        draw.text((10, 70), f"DISK: {psutil.disk_usage('/').percent}%", font=font, fill=255)  
-        draw.text((10, 90), f"TEM: {psutil.sensors_temperatures()['cpu_thermal'][0].current} c", font=font, fill=255)  
+    cpu_per = psutil.cpu_percent()
+    draw.text((10, 30), f"CPU: {cpu_per}%", font=font, fill=255)  
+    draw.text((10, 50), f"MEM: {psutil.virtual_memory().percent}%", font=font, fill=255)  
+    draw.text((10, 70), f"DISK: {psutil.disk_usage('/').percent}%", font=font, fill=255)  
+    draw.text((10, 90), f"TEM: {psutil.sensors_temperatures()['cpu_thermal'][0].current} c", font=font, fill=255)  
 
-        epd.displayPartial(epd.getbuffer(image))
+    epd.displayPartial(epd.getbuffer(image))
 
-    except KeyboardInterrupt:
-        print("กด Ctrl+C แล้วออกโปรแกรม")
-        exit()
-    except Exception as e:
-        #print(f"เกิดข้อผิดพลาด: {e}")
-        raise e
     #finally:
         #epd2in13_V4.epdconfig.module_exit()
 
@@ -131,24 +124,29 @@ print ("Initialize the CW2015 ......")
 
 
 while True:
+    try:
+        print ("++++++++++++++++++++")
+        print ("Voltage:%5.2fV" % readVoltage(bus))
+        fetch_screen("%i%%" % readCapacity(bus), GPIO.input(4) == GPIO.HIGH)
 
- print ("++++++++++++++++++++")
- print ("Voltage:%5.2fV" % readVoltage(bus))
- fetch_screen("%i%%" % readCapacity(bus), GPIO.input(4) == GPIO.HIGH)
- 
- if readCapacity(bus) == 100:
-        print ("Battery FULL")
- if readCapacity(bus) < 5:
-        print ("Battery LOW")
-
-
+        if readCapacity(bus) == 100:
+            print ("Battery FULL")
+        if readCapacity(bus) < 5:
+            print ("Battery LOW")
 
 
-#GPIO is high when power is plugged in
- if (GPIO.input(4) == GPIO.HIGH):       
-        print ("Power Adapter Plug In ") 
- if (GPIO.input(4) == GPIO.LOW):      
-        print ("Power Adapter Unplug")
 
- #time.sleep(0.02)
+
+        #GPIO is high when power is plugged in
+        if (GPIO.input(4) == GPIO.HIGH):       
+            print ("Power Adapter Plug In ") 
+        if (GPIO.input(4) == GPIO.LOW):      
+            print ("Power Adapter Unplug")
+
+    except KeyboardInterrupt:
+        print("กด Ctrl+C แล้วออกโปรแกรม")
+        exit()
+    except Exception as e:
+        #print(f"เกิดข้อผิดพลาด: {e}")
+        raise e
  
